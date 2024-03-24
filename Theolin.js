4ba0b2f46e852da657f5c08a90f80949f4d5fffd
TheolinPillay <164031332+TheolinPillay@users.noreply.github.com>
2024-03-24 13:29:35 +0200

    Add files via upload

---
+++ Theolin.js
@@ -0,0 +1,5 @@
+function changeParagraph(){
+	return "I am a person with storng will and desire to learn new things and takes initiative.";
+}
+const car = {make:"Volvo", Model: "XC90", paint:"Red", YoM:1998};
+let x = ["Manchester United", "Chelsea", "Luton Town", "Everton", "Wolverhampton Wnaderers"]
